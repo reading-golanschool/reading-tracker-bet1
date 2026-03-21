62ada1947f6cb953cc1a58b6cce9bd2331cf7727
romamsiligali <romamsiligali@gmail.com>
2026-03-21 20:23:47 +0200

    ב'1 - חיבור Apps Script

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 
 // ===== CONFIGURATION =====
 // Replace this URL with your Google Apps Script Web App URL
-const GOOGLE_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbwWKOqo3EyYeNJEd4v_74xy-SZSf6gxBshpXn9NTza7fMkC5SUL71Vm4iKlpXONhos4gg/exec';
+const GOOGLE_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbwraL1yEwUeGv_bJnWfyUyUnsu5cFNNLMTARVO8SLC10-Cf48ipEk00-k0GDY9Rv1J2/exec';
 
 // ===== INITIALIZE =====
 document.addEventListener('DOMContentLoaded', function() {
